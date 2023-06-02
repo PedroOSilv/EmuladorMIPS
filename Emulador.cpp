47ab2c1c5d9f4ffeb395f578fa0315e7bb8eaa47
Alexandre Libório da Fonseca <46246301+XandeLibs@users.noreply.github.com>
2023-06-02 11:47:31 -0400

    Adiciona instruções e arruma outras
    Arruma o funcionamento da instrução mult e adiciona algumas que faltavam, como branches

--- Emulador.cpp
+++ Emulador.cpp
@@ -1,3 +1,8 @@
+// #[1] a instrucao mul (0x1C) eh uma instrucao I, porem requer 3 registradores
+// achei que era um pseudointrucao, porem no MARS esta como se fosse e o binario gerado a inclui,
+// decidi implementar mesmo assim por compatibilidade com a demo
+// #[2] algo parecido ocorre com multu, que eh uma multiplicacao unsigned
+// de apenas dois registradores, porem eh considerada um operacao R
 #include <cstdio>
 #include <cstdlib>
 #include "Emulador.h"
@@ -50,7 +55,7 @@ int main(int argc, char const *argv[])
         instruction |= MEM[PC + 3] << 24;
 
         // executa a instrução
-        if(instruction == 0x00000000 || PC > 0x00003ffc){
+        if(PC > 0x00003ffc){
             break;
         }
         else{
@@ -141,6 +146,11 @@ void R_inst(word instruction){
         LO = REG[rs] / REG[rt];
         HI = REG[rs] % REG[rt];
         break;
+    case 0x19:
+        // multu, ver nota #[2]
+        LO = (unsigned)REG[rs] * (unsigned)REG[rt];
+        HI = ((uint64_t)REG[rs] * (uint64_t)REG[rt]) >> 32;
+        break;
     case 0x20:
         // add
         REG[rd] = REG[rs] + REG[rt];
@@ -173,7 +183,7 @@ void R_inst(word instruction){
         REG[rd] = REG[rs] < REG[rt];
         break;
     default:
-       printf("R type opcode nao reconhecido: PC: %d %d %X\n", PC, instruction, funct);
+       printf("R type opcode nao reconhecido: PC: %d %08X %X\n", PC, instruction, funct);
     }
 
     if(funct != 0x08){
@@ -186,11 +196,18 @@ void I_inst(word instruction, byte opcode){
     // extrai os campos
     byte rs = (instruction >> 21) & 0x1F;
     byte rt = (instruction >> 16) & 0x1F;
+    word temp_rs;
+    word &mul_res = temp_rs;
     int16_t immediate = instruction & 0xFFFF;
 
     // identifica o tipo de instrução
     switch (opcode)
     {
+    case 0x01:
+        //bqez
+        if(REG[rs] >+ 0){
+          PC += immediate << 2;
+        }
     case 0x04:
         // beq
         if(REG[rs] == REG[rt]){
@@ -200,6 +217,12 @@ void I_inst(word instruction, byte opcode){
     case 0x05:
         // bne
         if(REG[rs] != REG[rt]){
+            PC += immediate << 2;
+        }
+        break;
+    case 0x06:
+        // blez
+        if(REG[rs] <= 0){
             PC += immediate << 2;
         }
         break;
@@ -217,7 +240,7 @@ void I_inst(word instruction, byte opcode){
         break;
     case 0x0B:
         // sltiu
-        REG[rt] = (unsigned)REG[rs] < immediate;
+        REG[rt] = (unsigned)REG[rs] < (unsigned)immediate;
         break;
     case 0x0C:
         // andi
@@ -231,21 +254,33 @@ void I_inst(word instruction, byte opcode){
         // lui
         REG[rt] = immediate;
         break;
+    case 0x1C:
+        // mul $0, $1, $2; mult $1 e $2, escreve HIGH em $0, ver nota em #[1]
+        mul_res = (instruction >> 11) & 0x1F;
+        LO = REG[mul_res] = REG[rt] * REG[rs];
+        HI = ((int64_t)REG[rs] * (int64_t)REG[rt]) >> 32;
+        break;
     case 0x23:
         // lw
-        REG[rt] = MEM[REG[rs] + immediate] << 24;
-        REG[rt] |= MEM[REG[rs] + immediate + 1] << 16;
-        REG[rt] |= MEM[REG[rs] + immediate + 2] << 8;
-        REG[rt] |= MEM[REG[rs] + immediate + 3];
+        temp_rs = REG[rs];
+        REG[rt] = MEM[temp_rs + immediate] << 24;
+        REG[rt] |= MEM[temp_rs + immediate + 1] << 16;
+        REG[rt] |= MEM[temp_rs + immediate + 2] << 8;
+        REG[rt] |= MEM[temp_rs + immediate + 3];
         break;
     case 0x21:
         // lh
-        REG[rt] = MEM[REG[rs] + immediate] << 8;
-        REG[rt] |= MEM[REG[rs] + immediate + 1];
+        temp_rs = REG[rs];
+        REG[rt] = MEM[temp_rs + immediate] << 8;
+        REG[rt] |= MEM[temp_rs + immediate + 1];
         break;
     case 0x20:
         // lb
         REG[rt] = MEM[REG[rs] + immediate];
+        break;    
+    case 0x24:
+        // lbu
+        REG[rt] = (unsigned)MEM[REG[rs] + immediate];
         break;
     case 0x2B:
         // sw
@@ -264,7 +299,7 @@ void I_inst(word instruction, byte opcode){
         MEM[REG[rs] + immediate] = REG[rt];
         break;
     default:
-       printf("I type opcode nao reconhecido: PC: %d %d %X\n", PC, instruction, opcode);
+       printf("I type opcode nao reconhecido: PC: %d %08X %X\n", PC, instruction, opcode);
     }
 }
 
@@ -359,12 +394,12 @@ inline bool load_mem(char const* text, char const* data){
 }
 
 inline void print_reg_mem(){
-    word i;
+    /*word i;
 
     for(i = 0; i < 32; i++){
         printf("$%d\t%d\n", i, REG[i]);
     }
     for(i = 0; i < 1024; i++){
         printf("Mem[%x] 0x%x\t0x%x\t0x%x\t0x%x\n", i, MEM[i], MEM[i+1], MEM[i+2], MEM[i+3]);
-    }
+    }*/
 }
